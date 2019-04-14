941079cad98f5da689a32a103f92970f7aae3c79
heying <herby1030@outlook.com>
2019-04-14 18:09:55 +0800

    add one assertion in TC01

--- src/test/java/tests/TC01.java
+++ src/test/java/tests/TC01.java
@@ -3,28 +3,33 @@ package tests;
 import org.testng.annotations.Test;
 import pages.BasicPage;
 import utilities.Log;
+import utils.CustomizeAssertion;
 import utils.CustomizeFunctions;
 import utils.TestBase;
 
 public class TC01 extends TestBase {
     BasicPage basicPage;
     CustomizeFunctions customizeFunctions;
+    CustomizeAssertion customizeAssertion;
 
     @Test
     public void verify_TC01() throws Exception {
 
         basicPage = new BasicPage();
         customizeFunctions = new CustomizeFunctions();
+        customizeAssertion = new CustomizeAssertion();
 
         driver.get("https://www.baidu.com/");
 
         Log.info("Step 1: Enter keywords into search field");
         customizeFunctions.input(basicPage.inputOfSearch,"Automation test","Enter a keywords");
+        customizeAssertion.assertTrue(false,"verify it is true");
 
         Log.info("Step 2: Click on Submit button");
         customizeFunctions.click(basicPage.btnOfSubmit,"Click on submit button");
 
 
+        eventualAssert();
 
 
 
